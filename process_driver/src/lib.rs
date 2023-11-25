514b9e8f350740bdd51e4e337debe551c4ee1f1c
Paval-from-Belarus <star-war-sandakan@mail.ru>
2023-11-25 14:35:42 +0300

    save unstable changes

--- process_driver/src/lib.rs
+++ process_driver/src/lib.rs
@@ -1,79 +1,136 @@
 #![no_std]
+#![cfg_attr(feature = "nightly", feature(hint_must_use))]
+// #![deny(warnings)]
+// #![deny(clippy::all)]
+// #![deny(clippy::pedantic)]
+// #![deny(clippy::nursery)]
+// #![deny(clippy::cargo)]
+// #![deny(clippy::unnecessary_safety_doc)]
+
 extern crate alloc;
+
 #[cfg(not(test))]
 extern crate wdk_panic;
 
-use alloc::ffi::CString;
-use alloc::string::String;
-use core::mem::MaybeUninit;
-use core::{mem, slice};
-use static_assertions::const_assert;
-use wdk::println;
+
+use wdk::{nt_success, paged_code, println};
 #[cfg(not(test))]
 use wdk_alloc::WDKAllocator;
+use wdk_sys::{DRIVER_OBJECT, NTSTATUS, PCUNICODE_STRING, ULONG, WDFDEVICE, WDFDEVICE_INIT, WDFDRIVER, WDF_DRIVER_CONFIG, WDF_NO_HANDLE, WDF_NO_OBJECT_ATTRIBUTES, macros, *};
 
 #[cfg(not(test))]
 #[global_allocator]
 static GLOBAL_ALLOCATOR: WDKAllocator = WDKAllocator;
 
-use wdk_sys::ntddk::DbgPrint;
-use wdk_sys::{
-    DPFLTR_INFO_LEVEL, DRIVER_OBJECT, NTSTATUS, PCUNICODE_STRING, STATUS_SUCCESS, ULONG, WDFDEVICE,
-    WDFDEVICE_INIT, WDFDRIVER, WDF_DRIVER_CONFIG, WDF_NO_HANDLE, WDF_NO_OBJECT_ATTRIBUTES,
-    WDF_RELEASE_HARDWARE_ORDER_ON_FAILURE,
-};
-
-fn dbg_print(message: &str) {
-    let message = CString::new(message).unwrap();
-    unsafe {
-        DbgPrint(message.as_ptr());
-    }
-}
-
+/// `DriverEntry` function required by WDF
+///
+/// # Panics
+/// Can panic from unwraps of `CStrings` used internally
+///
+/// # Safety
+/// Function is unsafe since it dereferences raw pointers passed to it from WDF
+#[link_section = "INIT"]
 #[export_name = "DriverEntry"] // WDF expects a symbol with the name DriverEntry
-pub unsafe extern "system" fn driver_entry(
+extern "system" fn driver_entry(
     driver: &mut DRIVER_OBJECT,
     registry_path: PCUNICODE_STRING,
 ) -> NTSTATUS {
-    let status: NTSTATUS = STATUS_SUCCESS;
-    let mut driver_config = unsafe {
-        const CONFIG_SIZE: usize = mem::size_of::<WDF_DRIVER_CONFIG>();
-        const_assert!(CONFIG_SIZE <= ULONG::MAX as usize);
-        WDF_DRIVER_CONFIG {
-            Size: CONFIG_SIZE as ULONG,
-            EvtDriverDeviceAdd: Some(evt_driver_device_add),
-            ..WDF_DRIVER_CONFIG::default()
-        }
+    let mut driver_config = WDF_DRIVER_CONFIG {
+        Size: core::mem::size_of::<WDF_DRIVER_CONFIG>() as ULONG,
+        EvtDriverDeviceAdd: Some(evt_driver_device_add),
+        ..WDF_DRIVER_CONFIG::default()
     };
-    let driver_attributes = WDF_NO_OBJECT_ATTRIBUTES;
-    let driver_handle_output = WDF_NO_HANDLE.cast::<*mut wdk_sys::WDFDRIVER__>();
-    let wdf_driver_create_status = unsafe {
-        wdk_macros::call_unsafe_wdf_function_binding!(
+    let driver_handle_output = WDF_NO_HANDLE as *mut WDFDRIVER;
+
+    let nt_status = unsafe {
+        macros::call_unsafe_wdf_function_binding!(
             WdfDriverCreate,
-            driver as wdk_sys::PDRIVER_OBJECT,
+            driver as PDRIVER_OBJECT,
             registry_path,
-            driver_attributes,
+            WDF_NO_OBJECT_ATTRIBUTES,
             &mut driver_config,
             driver_handle_output,
         )
     };
-    let registry_path = String::from_utf16_lossy(unsafe {
-        slice::from_raw_parts(
-            (*registry_path).Buffer,
-            (*registry_path).Length as usize / core::mem::size_of_val(&(*(*registry_path).Buffer)),
-        )
-    });
-    println!("KMDF Driver Entry Complete! Driver Registry Parameter Key: {registry_path}");
-    status
+
+    if !nt_success(nt_status) {
+        println!("Error: WdfDriverCreate failed {nt_status:#010X}");
+        return nt_status;
+    }
+    nt_status
 }
 
+// #[link_section = "INIT"]
+// #[export_name = "DriverEntry"] // WDF expects a symbol with the name DriverEntry
+// pub unsafe extern "system" fn driver_entry(
+//     driver: &mut DRIVER_OBJECT,
+//     registry_path: PCUNICODE_STRING,
+// ) -> NTSTATUS {
+//     // This is an example of directly using DbgPrint binding to print
+//     let string = CString::new("Hello World!\n").unwrap();
+//     unsafe {
+//         DbgPrint(string.as_ptr());
+//     }
+//     driver.DriverUnload = Some(driver_exit);
+//
+//     let mut driver_config = {
+//         // const_assert required since clippy::cast_possible_truncation must be silenced because of a false positive (since it currently doesn't handle checking compile-time constants): https://github.com/rust-lang/rust-clippy/issues/9613
+//         const WDF_DRIVER_CONFIG_SIZE: usize = core::mem::size_of::<WDF_DRIVER_CONFIG>();
+//         const_assert!(WDF_DRIVER_CONFIG_SIZE <= ULONG::MAX as usize);
+//         let wdf_driver_config_size: ULONG;
+//         // truncation not possible because of above const_assert
+//         #[allow(clippy::cast_possible_truncation)]
+//         {
+//             wdf_driver_config_size = WDF_DRIVER_CONFIG_SIZE as ULONG;
+//         }
+//
+//         WDF_DRIVER_CONFIG {
+//             Size: wdf_driver_config_size,
+//             EvtDriverDeviceAdd: Some(evt_driver_device_add),
+//             ..WDF_DRIVER_CONFIG::default()
+//         }
+//     };
+//
+//     let driver_attributes = WDF_NO_OBJECT_ATTRIBUTES;
+//     let driver_handle_output = WDF_NO_HANDLE.cast::<*mut wdk_sys::WDFDRIVER__>();
+//
+//     let wdf_driver_create_ntstatus = unsafe {
+//         call_unsafe_wdf_function_binding!(
+//             WdfDriverCreate,
+//             driver as wdk_sys::PDRIVER_OBJECT,
+//             registry_path,
+//             driver_attributes,
+//             &mut driver_config,
+//             driver_handle_output,
+//         )
+//     };
+//
+//     // Translate UTF16 string to rust string
+//     let registry_path = String::from_utf16_lossy(unsafe {
+//         slice::from_raw_parts(
+//             (*registry_path).Buffer,
+//             (*registry_path).Length as usize / core::mem::size_of_val(&(*(*registry_path).Buffer)),
+//         )
+//     });
+//
+//     // It is much better to use the println macro that has an implementation in
+//     // wdk::print.rs to call DbgPrint. The println! implementation in
+//     // wdk::print.rs has the same features as the one in std (ex. format args
+//     // support).
+//     println!("KMDF Driver Entry Complete! Driver Registry Parameter Key: {registry_path}");
+//
+//     wdf_driver_create_ntstatus
+// }
+#[link_section = "PAGE"]
 extern "C" fn evt_driver_device_add(
-    driver: WDFDRIVER,
+    _driver: WDFDRIVER,
     mut device_init: *mut WDFDEVICE_INIT,
 ) -> NTSTATUS {
-    println!("Evt driver Entered!");
+    println!("EvtDriverDeviceAdd Entered!");
+
     let mut device_handle_output: WDFDEVICE = WDF_NO_HANDLE.cast();
-    let status = unsafe {
+
+    let ntstatus = unsafe {
         wdk_macros::call_unsafe_wdf_function_binding!(
             WdfDeviceCreate,
             &mut device_init,
@@ -81,48 +138,12 @@ extern "C" fn evt_driver_device_add(
             &mut device_handle_output,
         )
     };
-    println!("WdfDeviceCreate NTSTATUS: {status:#02x}");
-    status
+    println!("WdfDeviceCreate NTSTATUS: {ntstatus:#02x}");
+    ntstatus
 }
-
-extern "C" fn driver_exit(driver: *mut DRIVER_OBJECT) {
-    println!("Goodbye World!");
-    println!("Driver Exit Complete!");
-}
-// use wdk_alloc::WDKAllocator;
 //
-// #[cfg(not(test))]
-// #[global_allocator]
-// static GLOBAL_ALLOCATOR: WDKAllocator = WDKAllocator;
-// use wdk_sys::{
-//     DRIVER_OBJECT,
-//     NTSTATUS,
-//     PCUNICODE_STRING,
-// };
-//
-// // use wdk_sys::{DRIVER_OBJECT, NTSTATUS, PCUNICODE_STRING, WDF_NO_OBJECT_ATTRIBUTES};
-// //
-// #[export_name = "DriverEntry"] // WDF expects a symbol with the name DriverEntry
-// pub unsafe extern "system" fn driver_entry(
-//     driver: &mut DRIVER_OBJECT,
-//     registry_path: PCUNICODE_STRING,
-// ) -> NTSTATUS {
-// //     // let status: NTSTATUS;
-// //     // let config = MaybeUninit::<WDF_DRIVER_CONFIG >::uninit();
-// //     // WDF_DRIVER_CONFIG_INIT(config.as_mut_ptr(), )
-// //     // status = WdfDriverCreate(driver,
-// //     //                          registry_path,
-// //     //                          WDF_NO_OBJECT_ATTRIBUTES,
-// //     //
-// //     // )
-// //
-//     0
+// #[link_section = "INIT"]
+// extern "C" fn driver_exit(_driver: *mut DRIVER_OBJECT) {
+//     println!("Goodbye World!");
+//     println!("Driver Exit Complete!");
 // }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {}
-}
